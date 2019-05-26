2793f06c79a956d31c4b32e9f9b3b1f0934d981b
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-05-25 19:00:05 -0500

    Update u_sgota.py

--- code/u_sgota.py
+++ code/u_sgota.py
@@ -1,2 +1,16 @@
-x=test
-y=x
+import requests
+import re
+r =requests.get('https://api.github.com/repos/cj667113/simple_github_ota_updater/commits?path=code/u_sgota.py')
+collect=[]
+for x in r:
+    y=(re.search('"date"(.*)}',x))
+    try:
+        collect.append(y.group())
+    except:
+        pass
+        
+filtered=collect[0]
+filtered=(re.sub('"','',filtered))
+filtered=(re.sub('}','',filtered))
+filtered=(re.sub('date:','',filtered))
+print(filtered)
