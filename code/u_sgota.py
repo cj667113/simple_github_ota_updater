1020cf64d283f3a2b992e3f0577b34b37bf82687
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-05-25 19:20:46 -0500

    Update u_sgota.py

--- code/u_sgota.py
+++ code/u_sgota.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import datetime
 r =requests.get('https://api.github.com/repos/cj667113/simple_github_ota_updater/commits?path=code/u_sgota.py')
 collect=[]
 for x in r:
@@ -13,4 +14,12 @@ filtered=collect[0]
 filtered=(re.sub('"','',filtered))
 filtered=(re.sub('}','',filtered))
 filtered=(re.sub('date:','',filtered))
-print(filtered)
+d1 = datetime.datetime.now().date()
+d2 = datetime.datetime.strptime(filtered,"%Y-%m-%dT%H:%M:%SZ").date()
+print(d1)
+print(d2)
+
+if d1>d2:
+    print("true")
+else:
+    print("false")
