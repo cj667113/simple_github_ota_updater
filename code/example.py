e30f7902f58da08e9f388057daaafb695786cf77
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-06-07 15:49:27 -0500

    Update example.py

--- code/example.py
+++ code/example.py
@@ -1,4 +1,4 @@
 import sgota
 print("Example")
-sgota.sgota("https://api.github.com/repos/cj667113/simple_github_ota_updater/commits?path=code/example.py")
+sgota.update("https://api.github.com/repos/cj667113/simple_github_ota_updater/commits?path=code/example.py")
 print("Done")
