4d4c097dbcdabc2171f5567d8b6fa4283baa1e11
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-09-18 13:49:18 -0500

    Add files via upload

---
+++ code/usgota/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="usgota",
+    version="0.0.1",
+    author="Christopher M Johnston",
+    author_email="chris.johnston60.cj@gmail.com",
+    description="OTA Github Updater",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cj667113/simple_github_ota_updater",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
